a91f1aac481f764b4b8568ccfc0ca2186204459a
dadal00 <dylanadal.00@gmail.com>
2026-02-18 09:46:25 -0500

    Initial Commit

---
+++ CarController/CarControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarControllerApp.swift
+//  CarController
+//
+//  Created by Dylan Adal on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct CarControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarControllerTests/CarControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarControllerTests.swift
+//  CarControllerTests
+//
+//  Created by Dylan Adal on 2/18/26.
+//
+
+import Testing
+@testable import CarController
+
+struct CarControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
